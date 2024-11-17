a1e842dcee9279c7c668fc3c3d77420098c23dcc
Youssef Essouairi <rajawiiyoussef@gmail.com>
2024-11-17 23:50:59 +0100

    first day

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { delay, motion } from "framer-motion";
+import "./App.css";
+import { forwardRef } from "react";
+import { i } from "framer-motion/client";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const Button = forwardRef((props,ref) => {
+  //   return <button>...</button>
+  // });
+  // const MotionButton = motion(Button)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="flex items-center justify-center min-h-screen">
+      <motion.button
+        className="bg-black text-white px-3 py-2 rounded-md"
+        animate={{
+          backgroundColor: "#f00",
+          scaleX :2,
+          scaleY:2,
+          y:'50%'
+        }}
+        transition={{
+          repeatDelay:0.5,
+          repeatType :'reverse',
+          delay: 2,
+          ease : 'easeInOut',
+          repeat : 4, 
+          scaleX: {
+            delay :2,
+            repeatDelay :2,
+          }
+        }}
+        
+      >
+        Hello world
+      </motion.button>
+    </div>
+  );
 }
 
-export default App
+export default App;
